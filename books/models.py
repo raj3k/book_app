c74dd15f2d5ef0e857706c8fe8f99a2ab45f6e32
raj3k <rajkosky@gmail.com>
2022-04-02 18:37:57 +0200

    publication_date, isbn and pages_count fields changed

--- books/models.py
+++ books/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=100, help_text="The title of the book.")
-    publication_date = models.DateField(verbose_name="Publication date of the book")
-    isbn = models.CharField(max_length=15, verbose_name="ISBN number of the book")
-    pages_count = models.IntegerField(verbose_name="Number of pages in the book")
+    publication_date = models.CharField(max_length=20, verbose_name="Publication date of the book")
+    isbn = models.CharField(max_length=20, verbose_name="ISBN number of the book")
+    pages_count = models.IntegerField(verbose_name="Number of pages in the book", null=True, blank=True)
     cover_url = models.URLField(verbose_name="URL to book cover")
     pub_language = models.CharField(max_length=2, verbose_name="Language of the book publication")
     authors = models.ManyToManyField('Author', through="BookAuthor")
