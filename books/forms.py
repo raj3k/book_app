ea46c1593dfeb877098f26f8c763a0f506d48157
raj3k <rajkosky@gmail.com>
2022-04-02 09:44:38 +0200

    min_length changed to 2 chars

--- books/forms.py
+++ books/forms.py
@@ -2,4 +2,9 @@ from django import forms
 
 
 class SearchForm(forms.Form):
-    pass
+    search = forms.CharField(min_length=2)
+    search_in = forms.ChoiceField(required=False, choices=(
+        ("title", "Title"),
+        ("author", "Author"),
+        ("pub_language", "Language")
+    ))
