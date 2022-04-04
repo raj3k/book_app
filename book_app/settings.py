a67485b307a807b27a9f3c4e6078a1331ef1010a
raj3k <rajkosky@gmail.com>
2022-04-04 18:01:35 +0200

    removed SECRET KEY

--- book_app/settings.py
+++ book_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-9#4fgh-4%&m@&z+qgsogiz=u1m)g$k2e1kts3deupm__#t%j@7'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
